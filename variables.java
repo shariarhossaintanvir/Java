d614ce05ac0aaecaa3727c9d01aa52e4ecc23382
shariarhossaintanvir <bangboombro629@gmail.com>
2026-01-07 16:12:59 +0600

    Add Initial Files

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main (String[]args){
+        String name = "Shariar";
+        int age = 13;
+        double point = 3.1444;
+        float cg = 3.54F;
+        char ch = 's';
+        byte number = 30;
+       // boolean = True or False;
+    }
+}
